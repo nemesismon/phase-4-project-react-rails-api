4a89a71fcb9db082d8da3fa92336464e4622bebb
Michael <michael.w.kolb@gmail.com>
2022-12-12 14:57:15 -0500

    User punch lists rendering correctly with initial columns, started Project lists - setup table column names and data displaying, installed a toggle for list view or to create a new project

--- client/src/ProjectLists.js
+++ client/src/ProjectLists.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
 import './UserLists.css'
 
-function ProjectLists({ sessionUserData, setSessionUserData }) {
+function ProjectLists({ sessionUserData }) {
 
     const [sessionData, setSessionData] = useState(null)
     const [sessionProject, setSessionProject] = useState(false)
     const [createProject, setCreateProject] = useState(false)
+    const [projTitle, setProjTitle] = useState(null)
+    const [projAddress, setProjAddress] = useState(null)
+    const [projOwnerName, setProjOwnerName] = useState(null)
+    const [projCompBy, setProjCompBy] = useState(null)
 
     if (sessionUserData === null) {
         return (
@@ -21,18 +25,21 @@ function ProjectLists({ sessionUserData, setSessionUserData }) {
     const projectsList = () => {
         if (sessionData === null) {
             return (
-                <p>Unathorized - please login</p>
+                <h3>Unathorized - please login</h3>
             )
-        } else if (sessionData !== null && sessionProject === true) {
-            const projList = sessionData.map((item) => {
-            console.log(item)
+        } else if (sessionData !== null && sessionProject === false) {
             return (
+                <h3>Loading...</h3>
+            )
+        } else {
+            const projList = sessionData.map((item) => {
+            return (
+                <tbody>
                 <tr key={item.id}>
                     <td>{item.title}</td>
                     <td>{item.address}</td>
                     <td>{item.owner_name}</td>
                     {item.users.map((user) => {
-                        console.log(user)
                         return (
                             <>
                             <td>{user.company_name}</td>
@@ -44,13 +51,10 @@ function ProjectLists({ sessionUserData, setSessionUserData }) {
                     <td>{item.complete_by}</td>
                     <button>Mark Complete</button>
                 </tr>
+                </tbody>
             )
         })
         return projList
-        } else {
-            return (
-                <p>Loading...</p>
-            )
         }
     }
 
@@ -66,11 +70,11 @@ function ProjectLists({ sessionUserData, setSessionUserData }) {
                 <div>
                     <h1>Project List</h1>
                     <div align='right'>
-                    <input type="button" value="Create Project" /> 
+                        <input type="button" value="Create Project" onClick={() => setCreateProject(true)}/> 
                     </div>
-                    <br></br>              
+                    <br></br>      
                     <div>
-                            <table align='center'>
+                        <table align='center'>
                                 <tr>
                                     <th>Project Name</th>
                                     <th>Address</th>
@@ -81,25 +85,79 @@ function ProjectLists({ sessionUserData, setSessionUserData }) {
                                     <th>Complete by:</th>
                                     <th>Status</th>
                                 </tr>
-                                {projectsList()}
-                            </table>
-                        </div>
+                            {projectsList()}
+                        </table>
+                    </div>
                 </div>
             )
         }}
 
+        const createProjectForm = () => {
+            return (
+                <div>
+                    <h1>Add New Project</h1>
+                    <div align='right'>
+                        <input type="button" value="Projects List" onClick={() => setCreateProject(false)}/> 
+                    </div>
+                    <form>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        value={projTitle}
+                        onChange={(e) => setProjTitle(e.target.value)}
+                    />
+                    <ul></ul>
+                    <input
+                        type="text"
+                        name="address"
+                        placeholder="Address"
+                        value={projAddress} 
+                        onChange={(e) => setProjAddress(e.target.value)}
+                    />
+                    <ul></ul>
+                    <input
+                        type="text"
+                        name="owner name"
+                        placeholder="Owner Name"
+                        value={projOwnerName} 
+                        onChange={(e) => setProjOwnerName(e.target.value)}
+                    />
+                    <ul></ul>
+                    <input
+                        type="date"
+                        name="complete by"
+                        placeholder="Complete By"
+                        value={projCompBy} 
+                        onChange={(e) => setProjCompBy(e.target.value)}
+                    />
+                    <ul></ul>
+                    <button type="submit">Submit</button>
+                </form>
+                </div>
+            )
+        }
+
         const viewToggle = () => {
             if (createProject === false) {
-                sessionCheck()
+                return (
+                    <div>
+                    {sessionCheck()}
+                    </div>
+                )
             } else {
-
+                return (
+                    <div>
+                        {createProjectForm()}
+                    </div>
+                )
             }
         }
 
         return (
-            <>
+            <div>
             {viewToggle()}
-            </>
+            </div>
         )   
 }
 
