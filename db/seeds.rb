b79fc3e8eda71632c3c84a31fb440b75f69530c5
Michael <michael.w.kolb@gmail.com>
2023-01-25 22:30:14 -0500

    Cleaned up seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,41 @@
 
 puts 'Seeding...'
 
-User.create!(username: 'Timmy', password: 'timmy', password_confirmation: 'timmy', company_name: 'Tim Co', address: '1 St', trade_type: 'GC', point_of_contact: 'TIM', phone: '6145550987', email: 'tim@tim.com')
-User.create!(username: 'Tammy', password: 'tammy', password_confirmation: 'tammy', company_name: 'Tammy Co', address: '2 St', trade_type: 'Glass', point_of_contact: 'TAM-E', phone: '6145555743', email: 'tam@tam.com')
-User.create!(username: 'Corey', password: 'corey', password_confirmation: 'corey', company_name: 'Corey Co', address: '3 St', trade_type: 'Sitework', point_of_contact: 'Corey', phone: '6145552345', email: 'corey@corey.com')
+User.create!([{username: 'Timmy', password: 'timmy', password_confirmation: 'timmy', company_name: 'Tim Co', address: '1 St', trade_type: 'GC', point_of_contact: 'TIM', phone: '6145550987', email: 'tim@tim.com'}, 
+  {username: 'Tammy', password: 'tammy', password_confirmation: 'tammy', company_name: 'Tammy Co', address: '2 St', trade_type: 'Glass', point_of_contact: 'TAM-E', phone: '6145555743', email: 'tam@tam.com'},
+  {username: 'Corey', password: 'corey', password_confirmation: 'corey', company_name: 'Corey Co', address: '3 St', trade_type: 'Sitework', point_of_contact: 'Corey', phone: '6145552345', email: 'corey@corey.com'}
+])
 
-Project.create!(title: 'Retail building', address: '1 Boulevard', owner_name: 'RB Co', complete_by: '2023-05-01')
-Project.create!(title: 'Dental Office', address: '2 Boulevard', owner_name: 'Dental Co', complete_by: '2023-06-01')
-Project.create!(title: 'Mall', address: '3 Boulevard', owner_name: 'Mall Co', complete_by: '2023-07-01')
-Project.create!(title: 'Fast Food', address: '4 Boulevard', owner_name: 'FF Co', complete_by: '2023-08-01')
-Project.create!(title: 'Sit-down Restaurant', address: '5 Boulevard', owner_name: 'SDR Co', complete_by: '2023-09-01')
-Project.create!(title: 'Wings Wings Wings', address: '6 Boulevard', owner_name: 'WWW Co', complete_by: '2023-10-01')
-Project.create!(title: 'Bakery', address: '7 Boulevard', owner_name: 'Bakery Co', complete_by: '2023-11-01')
-Project.create!(title: 'Car Wash', address: '8 Boulevard', owner_name: 'CW Co', complete_by: '2023-12-01')
-Project.create!(title: 'Hospital', address: '9 Boulevard', owner_name: 'Hospital Co', complete_by: '2023-3-01')
-Project.create!(title: 'School Remodel', address: '10 Boulevard', owner_name: 'Town Name LSD', complete_by: '2023-04-01')
-Project.create!(title: 'Convention Center', address: '11 Boulevard', owner_name: 'CC Co', complete_by: '2023-05-01')
+puts 'User ✓'
 
-PunchItem.create!(user_id: 1, project_id: 1, task: 'Pull demo permit', area: 'Site', notes: 'Hello!!!', complete_by: '2023-03-01')
-PunchItem.create!(user_id: 2, project_id: 2, task: 'Turn in drawings', area: 'Offsite', notes: 'Hello!!!', complete_by: '2023-03-02')
-PunchItem.create!(user_id: 3, project_id: 3, task: 'Paint', area: 'Building', notes: 'Hello!!!', complete_by: '2023-03-03')
-PunchItem.create!(user_id: 1, project_id: 4, task: 'Sweep', area: 'Building', notes: 'Hello!!!', complete_by: '2023-03-04')
-PunchItem.create!(user_id: 2, project_id: 5, task: 'Look', area: 'Electrical', notes: 'Hello!!!', complete_by: '2023-03-05')
-PunchItem.create!(user_id: 3, project_id: 6, task: 'Watch', area: 'HVAC', notes: 'Hello!!!', complete_by: '2023-03-06')
-PunchItem.create!(user_id: 1, project_id: 7, task: 'Stand', area: 'Plumbing', notes: 'Hello!!!', complete_by: '2023-03-07')
-PunchItem.create!(user_id: 2, project_id: 8, task: 'Ponder', area: 'Site', notes: 'Hello!!!', complete_by: '2023-03-08')
-PunchItem.create!(user_id: 3, project_id: 9, task: 'Gaze', area: 'Offsite', notes: 'Hello!!!', complete_by: '2023-03-09')
-PunchItem.create!(user_id: 1, project_id: 10, task: 'Walk', area: 'Electrical', notes: 'Hello!!!', complete_by: '2023-03-10')
-PunchItem.create!(user_id: 2, project_id: 11, task: 'Stoop', area: 'HVAC',  notes: 'Hello!!!',complete_by: '2023-03-11')
+Project.create!([{title: 'Retail building', address: '1 Boulevard', owner_name: 'RB Co', complete_by: '2023-05-01'},
+  {title: 'Dental Office', address: '2 Boulevard', owner_name: 'Dental Co', complete_by: '2023-06-01'},
+  {title: 'Mall', address: '3 Boulevard', owner_name: 'Mall Co', complete_by: '2023-07-01'},
+  {title: 'Fast Food', address: '4 Boulevard', owner_name: 'FF Co', complete_by: '2023-08-01'},
+  {title: 'Sit-down Restaurant', address: '5 Boulevard', owner_name: 'SDR Co', complete_by: '2023-09-01'},
+  {title: 'Wings Wings Wings', address: '6 Boulevard', owner_name: 'WWW Co', complete_by: '2023-10-01'},
+  {title: 'Bakery', address: '7 Boulevard', owner_name: 'Bakery Co', complete_by: '2023-11-01'},
+  {title: 'Car Wash', address: '8 Boulevard', owner_name: 'CW Co', complete_by: '2023-12-01'},
+  {title: 'Hospital', address: '9 Boulevard', owner_name: 'Hospital Co', complete_by: '2023-3-01'},
+  {title: 'School Remodel', address: '10 Boulevard', owner_name: 'Town Name LSD', complete_by: '2023-04-01'},
+  {title: 'Convention Center', address: '11 Boulevard', owner_name: 'CC Co', complete_by: '2023-05-01'}
+])
+
+puts 'Project ✓'
+
+PunchItem.create!([{user_id: 1, project_id: 1, task: 'Pull demo permit', area: 'Site', notes: 'Hello!!!', complete_by: '2023-03-01'},
+  {user_id: 2, project_id: 2, task: 'Turn in drawings', area: 'Offsite', notes: 'Hello!!!', complete_by: '2023-03-02'},
+  {user_id: 3, project_id: 3, task: 'Paint', area: 'Building', notes: 'Hello!!!', complete_by: '2023-03-03'},
+  {user_id: 1, project_id: 4, task: 'Sweep', area: 'Building', notes: 'Hello!!!', complete_by: '2023-03-04'},
+  {user_id: 2, project_id: 5, task: 'Look', area: 'Electrical', notes: 'Hello!!!', complete_by: '2023-03-05'},
+  {user_id: 3, project_id: 6, task: 'Watch', area: 'HVAC', notes: 'Hello!!!', complete_by: '2023-03-06'},
+  {user_id: 1, project_id: 7, task: 'Stand', area: 'Plumbing', notes: 'Hello!!!', complete_by: '2023-03-07'},
+  {user_id: 2, project_id: 8, task: 'Ponder', area: 'Site', notes: 'Hello!!!', complete_by: '2023-03-08'},
+  {user_id: 3, project_id: 9, task: 'Gaze', area: 'Offsite', notes: 'Hello!!!', complete_by: '2023-03-09'},
+  {user_id: 1, project_id: 10, task: 'Walk', area: 'Electrical', notes: 'Hello!!!', complete_by: '2023-03-10'},
+  {user_id: 2, project_id: 11, task: 'Stoop', area: 'HVAC',  notes: 'Hello!!!',complete_by: '2023-03-11'}
+])
+
+puts 'PunchItem ✓'
 
 puts 'Seeding complete.'
